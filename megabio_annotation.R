109e005588cfc3cf8aaebadf7f4d58abfd8a643a
Rodrigo O. Polo <r.ortegapolo@uleth.ca>
2017-08-09 17:05:17 -0600

    Updates to R script

--- megabio_annotation.R
+++ megabio_annotation.R
@@ -12,16 +12,23 @@ megabioNew <- megabioCSU[megabioCSU$ProtID %in% aafcProtIDs$V1,]
 
 write.csv(megabioNew, 'megabio_AAFC_v0.1_annotations.csv', row.names = FALSE)
 
+# Read file generated with Pandas that includes the ProtIDs and the new headers
 megabioNewHeaders <- read.csv('megabio_updated_headers.csv')
 
+# Merge data frames by Protein ID
 megabioMerged <- merge(megabioNew, megabioNewHeaders, by="ProtID")
 
+# Export to CSV
 write.csv(megabioMerged, 'megabio_AAFC_v0.1_Merged.csv', row.names = FALSE)
 
-megabioNewClean <- megabioMerged %>% select(-c("Header", "ProtID"))
+# Remove unnecessary columns for Galaxy workflow
 
 megabioNewClean <- megabioMerged %>% select(-c(.data$Header, .data$ProtID))
 
+# Rename dataframe columns
+
 names(megabioNewClean) <- c('gene', 'class', 'mechanism', 'header')
+
+# Export to CSV: this is the "official" MegaBio
 
 write.csv(megabioNewClean, 'megabio_AAFC_v0.1_annotations_NewHeader.csv')
