74a2d2f9f6d11b35f305318c8b37515b084c079a
Rodrigo O. Polo <r.ortegapolo@uleth.ca>
2017-08-09 16:39:08 -0600

    Structure script for building MegaBio db

--- buildingMegabio.py
+++ buildingMegabio.py
@@ -1,14 +1,69 @@
 from Bio import SeqIO
+import argparse
 
-megabio_accessions = open('megabio_accessions.txt').read().splitlines()
+def arguments():
+    parser = argparse.ArgumentParser(description='Program for building the MegaBio database from exisiting aminoacid records')
 
-megabio_nt = [rec for rec in SeqIO.parse('megabio_database_cds.fasta', 'fasta')]
+    parser.add_argument()
 
-matches = []
+    return parser.parse_args()
 
-for m in megabio_accessions:
-    for n in megabio_nt:
-        for m in n.id:
-            matches.append(n)
+def current_megabio(megabio):
 
+    """ Read file with MegaBio accessions, and read CDS database extracted with edirect """
 
+    # TODO: Check if data can be downloaded and parsed with BioPython. Consider snakemake.
+
+    # TODO: Double check if this needs to have a closing handle
+
+    megabio_accessions = open('megabio_accessions.txt').read().splitlines()
+
+    megabio_nt = [rec for rec in SeqIO.parse('megabio_database_cds.fasta', 'fasta')]
+
+    return megabio_accessions, megabio_nt
+
+def accession_matches(megabio_accessions, megabio_nt):
+
+    """ Returns list of CDS records matched by the MegaBio aminoacid accessions"""
+
+    matches = []
+
+    for m in megabio_accessions:
+        for n in megabio_nt:
+            for m in n.description:
+                matches.append(n)
+
+    return matches
+
+def unique_matches(matches):
+    """ Returns a dictionary with record IDs as keys and records as values """
+
+    matchDict = {}
+
+    for m in matches:
+        matchDict[m.id] = m
+
+    return matchDict
+
+def export_matches(matchDict):
+
+    """ Save matched records as FASTA file """
+
+    toSave = [v for v in matchDict.values()]
+
+    SeqIO.write(toSave, 'megabio_AAFC.fasta', 'fasta')
+
+def main():
+
+    args = arguments()
+
+    current_records = current_megabio(megabio)
+
+    matched_by_current = accession_matches(current_records)
+
+    unique_cds = unique_matches(matched_by_current)
+
+    export_matches(unique_matches)
+
+if __name__ == '__main__':
+    main()
